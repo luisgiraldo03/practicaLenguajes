e1b82d3e7b9e4c3c0cbefa224934d79d049f3d35
luisgiraldo03 <legiraldoj@eafit.edu.co>
2017-11-15 10:15:39 -0500

    modificaciones

--- practicaFinal.cpp
+++ practicaFinal.cpp
@@ -5,11 +5,11 @@
 
 using namespace std;
 
-void menu();
-void encriptar();
-void desencriptarTodo();
-void desencriptarEspecifico();
-void mostrar();
+void menu(); //menú numérico
+void encriptar();//método para encriptar el texto
+void desencriptarTodo();//método que desencripta toda la información del archivo de texto
+void desencriptarEspecifico();//método que desencripta un texto específico
+void mostrar();//método que muestra toda la información encriptada
 
 int main(){
 	menu();
@@ -54,38 +54,38 @@ void encriptar(){
 	ofstream archivo, archivoEncriptado;
 	char rpt;
 	
-	archivo.open("archivo.txt",ios::app);
-	archivoEncriptado.open("archivoEncriptado.txt",ios::app);
+	archivo.open("archivo.txt",ios::app);//archivo de texto que guarda la información sin encriptar 
+	archivoEncriptado.open("archivoEncriptado.txt",ios::app);//archivo que guarda la información encriptada
 	
-	if(archivo.fail()){
+	if(archivo.fail()){//si el archivo falla
 		cout<<"Error al abrir el archivo"<<endl;
-		exit(1);
+		exit(1);//función que se utiliza gracias a la librería stdlib.h
 	}
 	do{
 		fflush(stdin);
-		cout<<"escriba texto para encriptar"<<endl;
+		cout<<"Escriba texto para encriptar"<<endl;
 		getline(cin, frase);
-		int aux = frase.length();
+		int aux = frase.length();//asignamos a aux el tamaño de la frase
 		char texto[aux];
 		for(int i=0;i<frase.length(); i++){
-			texto[i] = frase[i];
+			texto[i] = frase[i];//asignamos a la primera posición la primera letra de la palabra
 		}
-		archivo<<frase;
-		archivo<<"\n";
+		archivo<<frase;//agregar texto sin encriptar al archivo
+		archivo<<"\n";//agregar un salto de linea al texto
 		
-		while(texto[i] !='\0'){
-			texto[i] = texto[i]+12-4;
-			archivoEncriptado<<texto[i];
-			i++;
+		while(texto[i] !='\0'){//mientras el texto sea diferente a vacío
+			texto[i] = texto[i]+12-4;//encriptamos cada palabra 
+			archivoEncriptado<<texto[i];//guardamos la palabra en el archivo encriptado
+			i++;//incrementamos i en 1;
 		}
 		i=0;
-		archivoEncriptado<<"\n";
-		cout<<"desea agregar otra frase? (s/n)"<<endl;
+		archivoEncriptado<<"\n";//agregamos un salto de linea a el archivo encriptado
+		cout<<"desea agregar otra frase? Si=(ingrese 's') No(Ingrese 'n')"<<endl;
 		cin>>rpt;
 	
 		
 	}while((rpt == 'S')||(rpt == 's'));
-	archivo.close();
+	archivo.close();//cerramos el archivo
 	cout<<"\n============================================================="<<endl<<endl;
 }
 
@@ -94,25 +94,25 @@ void desencriptarTodo(){
 	string frase;
 	int i=0;
 
-	archivo.open("archivoEncriptado.txt",ios::in);
+	archivo.open("archivoEncriptado.txt",ios::in);//abrimos el archivo encriptado
 	
-	if(archivo.fail()){
+	if(archivo.fail()){//si el archivo falla
 		cout<<"No se puede abrir el archivo";
 		exit(1);
 	}
 	cout<<"=======================Texto Desencriptado===================================="<<endl;
-	while(!archivo.eof()){
+	while(!archivo.eof()){//minetras el archivo no esté vacío
 		getline(archivo,frase);
 		
-		while(frase[i] != '\0'){
-			frase[i] = frase[i]-12+4;
+		while(frase[i] != '\0'){//recorremos el archivo encriptado
+			frase[i] = frase[i]-12+4;//desencriptamos
 			i++;
 		}
 		i=0;
-		cout<< frase <<endl;	
+		cout<< frase <<endl;//imprimimos 
 	}
 	cout<<"=======================Texto Desencriptado===================================="<<endl;
-	archivo.close();
+	archivo.close();//cerramos el archivo
 }
 
 void desencriptarEspecifico(){
@@ -121,17 +121,16 @@ void desencriptarEspecifico(){
 	int i=0;
 	
 	cout<<"Ingrese texto encriptado para desencriptar"<<endl;
-	cin>>frase;
+	cin>>frase;//escribimos (o copiamos) la frase encriptada
 	
-	while(frase[i] !='\0'){
-		frase[i] = frase[i]-12+4;
+	while(frase[i] !='\0'){//recorremos las lineas
+		frase[i] = frase[i]-12+4;//desencriptamos
 		i++;
 	}
 	i=0;
 	cout<<"\n===========================Texto desencriptado================================="<<endl;
-	cout<<frase<<endl;
+	cout<<frase<<endl;//imprimimos el texto desencriptado
 	cout<<"===========================Texto desencriptado================================="<<endl<<endl;
-	
 }
 
 void mostrar(){
@@ -145,12 +144,12 @@ void mostrar(){
 		exit(1);
 	}
 		cout<<"\n===========================Texto Encriptado================================="<<endl;
-		while(!archivo.eof()){
+		while(!archivo.eof()){//mientras el archivo no este vacío
 			getline(archivo,frase);
-			cout<<frase<<endl;
+			cout<<frase<<endl;//imprimimos las lineas del archivo
 		}
 		cout<<"===========================Texto Encriptado================================="<<endl<<endl;
-	archivo.close();
+	archivo.close();//cerramos el archivo
 }
 
 
